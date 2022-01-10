0ee00ad09d03987bdf477b5240d4ebf1c9fca0c6
Naomi van Es <70514560+Naomivanes@users.noreply.github.com>
2022-01-10 16:11:15 +0100

    Moved Naturalis specimen data to a new table named naturalis_specimen, used to be included in species_marker

---
+++ scripts/classes/naturalis_specimen.py
@@ -0,0 +1,15 @@
+from scripts.classes.imports import *
+
+
+class NaturalisSpecimen(Base):
+    __tablename__ = 'naturalis_specimen'
+    naturalis_id = Column(String, primary_key=True)
+    species_id = Column(Integer, ForeignKey('nsr_species.species_id'),
+                        primary_key=True)
+    nsrspecies = relationship("NsrSpecies",
+                              backref=backref("NaturalisSpecimens",
+                                              cascade="all, delete"))
+
+    def __repr__(self):
+        return "<NaturalisSpecimen(name='%s')>" % (
+                         self.naturalis_id)

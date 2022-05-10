87bd83302cd844ff3e014dfff71b6cf6ba562f18
Rutger Vos <rutgeraldo@gmail.com>
2022-05-10 23:45:57 +0200

    added indexing for lookups of nsr_id and canonical_name

--- src/arise/barcode/metadata/orm/nsr_species.py
+++ src/arise/barcode/metadata/orm/nsr_species.py
@@ -5,8 +5,8 @@ class NsrSpecies(Base):
     __tablename__ = 'nsr_species'
 
     species_id = Column(Integer, primary_key=True)
-    nsr_id = Column(String)
-    canonical_name = Column(String)
+    nsr_id = Column(String, index=True)
+    canonical_name = Column(String, index=True)
 
     def __repr__(self):
         return "<NsrSpecies(species_name='%s')>" % (

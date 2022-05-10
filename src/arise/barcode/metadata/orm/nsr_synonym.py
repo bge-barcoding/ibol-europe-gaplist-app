d3e53f8c5739d8524f4ae7f8868960b0b67df254
Rutger Vos <rutgeraldo@gmail.com>
2022-05-10 23:47:59 +0200

    added indexing, removed backref (for now?)

--- src/arise/barcode/metadata/orm/nsr_synonym.py
+++ src/arise/barcode/metadata/orm/nsr_synonym.py
@@ -4,19 +4,11 @@ from arise.barcode.metadata.orm.imports import *
 class NsrSynonym(Base):
     __tablename__ = 'nsr_synonym'
     synonym_id = Column(Integer, primary_key=True)
-    synonym_name = Column(String)
-    identification_reference = Column(String)
-    species_id = Column(Integer, ForeignKey('nsr_species.species_id'),
-                        primary_key=True)
-    nsrspecies = relationship("NsrSpecies",
-                              backref=backref("nsr_synonym",
-                                              cascade="all, delete"))
+    synonym_name = Column(String, index=True)
+    species_id = Column(Integer, ForeignKey('nsr_species.species_id'))
+    nsr_id = Column(String, ForeignKey('nsr_species.nsr_id'))
 
     def __repr__(self):
         return "<NsrSynonym(synonym_name='%s')>" % (
                          self.synonym_name)
-
-
-
-
 

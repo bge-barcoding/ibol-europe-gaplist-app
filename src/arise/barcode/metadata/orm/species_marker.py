65bab1b103d754650966c181b8d8a7ab649ed1c8
Rutger Vos <rutgeraldo@gmail.com>
2022-05-11 19:21:00 +0200

    cleaned up, commented barcode table

--- src/arise/barcode/metadata/orm/species_marker.py
+++ src/arise/barcode/metadata/orm/species_marker.py
@@ -1,26 +1,28 @@
 from arise.barcode.metadata.orm.imports import *
 
 
-class SpeciesMarker(Base):
-    __tablename__ = 'species_marker'
+class Barcode(Base):
+    __tablename__ = 'barcode'
 
-    species_marker_id = Column(Integer, primary_key=True)
-    sequence_id = Column(Integer)
+    # auto-incrementing int, primary key
+    barcode_id = Column(Integer, primary_key=True)
+
+    # foreign key to species
     species_id = Column(Integer, ForeignKey('nsr_species.species_id'))
-    nsrspecies = relationship("NsrSpecies", backref=backref("SpeciesMarkers", cascade="all, delete"))
+
+    # foreign key to database, e.g. BOLD
     database_id = Column(Integer, ForeignKey('database.database_id'))
-    database = relationship("Database", backref=backref("SpeciesMarkers", cascade="all, delete"))
+
+    # foreign key to marker names list
     marker_id = Column(Integer, ForeignKey('marker.marker_id'))
-    marker = relationship("Marker", backref=backref("SpeciesMarkers", cascade="all, delete"))
-    genbank_accession = Column(String(20))
-    bin_uri = Column(String(13))
+
+    # whatever external ID is available, could be:
+    # - http://www.boldsystems.org/index.php/Public_RecordView?processid=$ID
+    # - http://ncbi.nlm.nih.gov/nuccore/$ID
+    # - internal ID that doesn't resolve
+    external_id = Column(String)
 
     def __repr__(self):
         return "<SpeciesMarker(sequence_id='%s')>" % (
                          self.sequence_id)
-
-
-#IDEA: method(s) that retrieves the sequence of the object using its sequence id and database_id
-
-#IDEA: add species
 

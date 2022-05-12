7842c0d9466892979dd148c180b04a3074239195
Rutger Vos <rutgeraldo@gmail.com>
2022-05-12 14:20:41 +0200

    added fields for filtering data

--- src/arise/barcode/metadata/orm/naturalis_specimen.py
+++ src/arise/barcode/metadata/orm/naturalis_specimen.py
@@ -1,15 +1,17 @@
 from arise.barcode.metadata.orm.imports import *
 
 
-class NaturalisSpecimen(Base):
-    __tablename__ = 'naturalis_specimen'
-    naturalis_id = Column(String, primary_key=True)
-    species_id = Column(Integer, ForeignKey('nsr_species.species_id'),
-                        primary_key=True)
-    nsrspecies = relationship("NsrSpecies",
-                              backref=backref("NaturalisSpecimens",
-                                              cascade="all, delete"))
+class Specimen(Base):
+    __tablename__ = 'specimen'
+    specimen_id = Column(Integer, primary_key=True)
+    catalognum = Column(String)
+    institution_storing = Column(String)
+    identification_provided_by = Column(String)
+    species_id = Column(Integer, ForeignKey('nsr_species.species_id'), primary_key=True)
+
+    # relationship
+    nsrspecies = relationship("NsrSpecies", backref=backref("Specimen", cascade="all, delete"))
 
     def __repr__(self):
-        return "<NaturalisSpecimen(name='%s')>" % (
-                         self.naturalis_id)
+        return "<Specimen(name='%s')>" % (
+                         self.specimen_id)

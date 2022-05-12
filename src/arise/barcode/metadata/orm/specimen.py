dfb6643727be5711534b3537921e18c803a426a7
Rutger Vos <rutgeraldo@gmail.com>
2022-05-12 21:39:36 +0200

    PK now autoincrements, added indexes on columns

--- src/arise/barcode/metadata/orm/specimen.py
+++ src/arise/barcode/metadata/orm/specimen.py
@@ -3,11 +3,11 @@ from arise.barcode.metadata.orm.imports import *
 
 class Specimen(Base):
     __tablename__ = 'specimen'
-    specimen_id = Column(Integer, primary_key=True)
-    catalognum = Column(String)
-    institution_storing = Column(String)
-    identification_provided_by = Column(String)
-    species_id = Column(Integer, ForeignKey('nsr_species.species_id'), primary_key=True)
+    specimen_id = Column(Integer, primary_key=True, autoincrement=True)
+    catalognum = Column(String, index=True)
+    institution_storing = Column(String, index=True)
+    identification_provided_by = Column(String, index=True)
+    species_id = Column(Integer, ForeignKey('nsr_species.species_id'))
 
     # relationship
     nsrspecies = relationship("NsrSpecies", backref=backref("Specimen", cascade="all, delete"))

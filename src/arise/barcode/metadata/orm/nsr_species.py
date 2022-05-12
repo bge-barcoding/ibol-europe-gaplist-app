560149a8f2d15762a485b63fcb44f079099a0250
Rutger Vos <rutgeraldo@gmail.com>
2022-05-12 21:40:02 +0200

    PK now autoincrements

--- src/arise/barcode/metadata/orm/nsr_species.py
+++ src/arise/barcode/metadata/orm/nsr_species.py
@@ -4,7 +4,7 @@ from arise.barcode.metadata.orm.imports import *
 class NsrSpecies(Base):
     __tablename__ = 'nsr_species'
 
-    species_id = Column(Integer, primary_key=True)
+    species_id = Column(Integer, primary_key=True, autoincrement=True)
     canonical_name = Column(String, index=True)
 
     def __repr__(self):

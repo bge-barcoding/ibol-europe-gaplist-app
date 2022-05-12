381c3c6874e71cd38d111b169a8527541cfac944
Rutger Vos <rutgeraldo@gmail.com>
2022-05-12 21:40:25 +0200

    added IntEnum for data sources

--- src/arise/barcode/metadata/orm/imports.py
+++ src/arise/barcode/metadata/orm/imports.py
@@ -2,4 +2,12 @@ from sqlalchemy import Column, Integer, String, Float
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship, backref
+from enum import IntEnum
 Base = declarative_base()
+
+class DataSource(IntEnum):
+    NATURALIS = 1
+    BOLD = 2
+    NCBI = 3
+    WFBI = 4
+    UNITE = 5

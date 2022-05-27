13ebf48c6aab7ae90502fa3ed4fb97bebfce9127
Rutger Vos <rutgeraldo@gmail.com>
2022-05-27 16:43:21 +0200

    Added match_specimen method here (from load BOLD) so it can be used by Klasse loader.

--- src/arise/barcode/metadata/orm/specimen.py
+++ src/arise/barcode/metadata/orm/specimen.py
@@ -11,6 +11,19 @@ class Specimen(Base):
 
     barcodes = relationship('Barcode', backref=backref("specimen", cascade="all, delete"))
 
+    # find or create specimen object
+    @classmethod
+    def match_specimen(cls, species_id, catalognum, institution_storing, identification_provided_by, session):
+        specimen = session.query(Specimen).filter(Specimen.species_id == species_id, Specimen.catalognum == catalognum,
+                                                  Specimen.institution_storing == institution_storing,
+                                                  Specimen.identification_provided_by == identification_provided_by).first()
+        if specimen is None:
+            specimen = Specimen(species_id=species_id, catalognum=catalognum, institution_storing=institution_storing,
+                                identification_provided_by=identification_provided_by)
+            session.add(specimen)
+            session.flush()
+        return specimen
+
     def __repr__(self):
         return "<Specimen(name='%s')>" % (
                          self.specimen_id)

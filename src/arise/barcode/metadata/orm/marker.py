a684d9459ed68f5b66e8456b3cede45cd5dbcd2c
Rutger Vos <rutgeraldo@gmail.com>
2022-05-27 16:45:07 +0200

    Moved match_marker here from BOLD loader so that Klasse loader can use it.

--- src/arise/barcode/metadata/orm/marker.py
+++ src/arise/barcode/metadata/orm/marker.py
@@ -9,6 +9,16 @@ class Marker(Base):
 
     barcodes = relationship('Barcode', backref=backref("marker", cascade="all, delete"))
 
+    # find or create marker object
+    @classmethod
+    def match_marker(cls, marker_name, session):
+        marker = session.query(Marker).filter(Marker.marker_name == marker_name).first()
+        if marker is None:
+            marker = Marker(marker_name=marker_name)
+            session.add(marker)
+            session.flush()
+        return marker
+
     def __repr__(self):
         return "<Marker(marker_name='%s')>" % (
             self.marker_name)

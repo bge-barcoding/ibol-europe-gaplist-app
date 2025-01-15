4a94d44a54b13714a710279e9afc8710f0f7ea4e
Pierre-etienne <pierreetienne.cholley@naturalis.nl>
2025-01-15 18:16:56 +0100

    add output param for BOLD gz file script

--- src/arise/barcode/metadata/util/generate_bold_gz_files.py
+++ src/arise/barcode/metadata/util/generate_bold_gz_files.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 import pandas as pd
 import gzip
@@ -12,6 +13,7 @@ if __name__ == '__main__':
                         help="BOLD data package file (tar.gz), downloadable at https://www.boldsystems.org/index.php/datapackages")
     parser.add_argument('label', help="label for output filename")
     parser.add_argument('--read-only', dest='read_only', action='store_true', help="do not write data to file")
+    parser.add_argument('--output-dir', dest='output_dir', help="Location of the output gz files")
 
     args = parser.parse_args()
 
@@ -24,9 +26,23 @@ if __name__ == '__main__':
     entry_plantea = 0
     entry_animalia = 0
     entry_other = 0
-    with gzip.open('BOLD_%s_Fungi.tsv.gz' % args.label, 'wt', encoding='UTF-8') as fw_f, \
-         gzip.open('BOLD_%s_Plantae.tsv.gz' % args.label, 'wt', encoding='UTF-8') as fw_p, \
-         gzip.open('BOLD_%s_Animalia.tsv.gz' % args.label, 'wt', encoding='UTF-8') as fw_a:
+
+    try:
+        os.makedirs(args.output_dir, exist_ok=True)
+    except:
+        print(f"Could not create output directory '{args.output_dir}'")
+        exit(1)
+
+    fungi_file = os.path.join(args.output_dir, 'BOLD_%s_Fungi.tsv.gz' % args.label)
+    assert not os.path.isfile(fungi_file), f"Fungi file '{fungi_file}' already exist"
+    plantae_file = os.path.join(args.output_dir, 'BOLD_%s_Plantae.tsv.gz' % args.label)
+    assert not os.path.isfile(fungi_file), f"Plantae file '{plantae_file}' already exist"
+    animalia_file = os.path.join(args.output_dir, 'BOLD_%s_Animalia.tsv.gz' % args.label)
+    assert not os.path.isfile(fungi_file), f"Animalia file '{animalia_file}' already exist"
+
+    with gzip.open(fungi_file, 'wt', encoding='UTF-8') as fw_f, \
+         gzip.open(plantae_file, 'wt', encoding='UTF-8') as fw_p, \
+         gzip.open(animalia_file, 'wt', encoding='UTF-8') as fw_a:
 
         utf8reader = codecs.getreader('UTF-8')
         fp = utf8reader(tar.extractfile(bold_tsv_file))

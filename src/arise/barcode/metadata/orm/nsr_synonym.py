df4760b2cde33fcc1b64845da7f696b7ac63d4c8
Pierre-etienne <pierreetienne.cholley@naturalis.nl>
2025-01-15 18:10:00 +0100

    fix: add missing code: add node id and nsr id in synonym table

--- src/arise/barcode/metadata/orm/nsr_synonym.py
+++ src/arise/barcode/metadata/orm/nsr_synonym.py
@@ -1,5 +1,5 @@
 from orm.common import Base
-from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy import Column, Integer, String, Float, Boolean, ForeignKey
 from sqlalchemy.orm import validates, relationship, backref
 from sqlalchemy.schema import UniqueConstraint
 import logging
@@ -14,20 +14,27 @@ class NsrSynonym(Base):
 
     __tablename__ = 'nsr_synonym'
     id = Column(Integer, primary_key=True, autoincrement=True)
+    nsr_id = Column(String(100))
     name = Column(String(50), index=True, nullable=False)
-    taxonomic_status = Column(String(15), nullable=False)
-    species_id = Column(Integer, ForeignKey('nsr_species.id'), nullable=False)
-    # species = relationship('NsrSpecies', backref=backref("synonyms", cascade="all, delete"))
+    taxonomic_status = Column(String(2))
+    node_id = Column(Integer, ForeignKey('nsr_species.id'), nullable=True)
+    species_id = Column(Integer, ForeignKey('nsr_species.id'), nullable=True)
 
-    __table_args__ = (UniqueConstraint('name', 'species_id', name='uc_synonym'),)
+    __table_args__ = (UniqueConstraint('name', 'node_id', name='uc_synonym'),)
 
     @classmethod
-    def insert_synonym(cls, session, name, taxonomic_status, species_id):
+    def insert_synonym(cls, session, name, nsr_id, taxonomic_status, node_id, species_id):
         created = False
-        synonym = session.query(NsrSynonym).filter(NsrSynonym.name == name, NsrSynonym.species_id == species_id).first()
+        synonym = session.query(NsrSynonym).filter(NsrSynonym.name == name, NsrSynonym.node_id == node_id).first()
         if not synonym:
             # make sure the name of the species is different from NAME before inserting
-            synonym = NsrSynonym(name=name, taxonomic_status=taxonomic_status, species_id=species_id)
+            synonym = NsrSynonym(
+                nsr_id=nsr_id,
+                name=name,
+                taxonomic_status=taxonomic_status,
+                node_id=node_id,
+                species_id=species_id
+            )
             session.add(synonym)
             session.flush()
             created = True

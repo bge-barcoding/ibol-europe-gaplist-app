c7d06af4f119c32581db8176d6e10717bcce2531
Rutger Vos <rutgeraldo@gmail.com>
2025-03-28 18:38:20 +0100

    add: script to create and load barcode metadata database

---
+++ make_bge_db.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# activate the env first:
+# conda activate target-list
+# export PYTHONPATH=./src/
+
+# create the directory if it does not exist
+if [ ! -d "data/sqlite/ignored" ]; then
+  mkdir -p data/sqlite/ignored
+fi
+
+# remove the file if it exists
+rm -f data/sqlite/ignored/arise-barcode-metadata.db
+
+# create the database
+python src/arise/barcode/metadata/util/arise_create_barcode_metadata_db.py -outfile data/sqlite/ignored/arise-barcode-metadata.db
+
+# fetch the backbone data using curl from:
+# https://raw.githubusercontent.com/bge-barcoding/gaplist-data/refs/heads/main/data/Gap_list_all_updated.csv
+curl -L https://raw.githubusercontent.com/bge-barcoding/gaplist-data/refs/heads/main/data/Gap_list_all_updated.csv -o data/input_files/Gap_list_all_updated.csv
+
+# load the backbone data
+python src/arise/barcode/metadata/util/bge_load_targetlist.py \
+  --db data/sqlite/ignored/arise-barcode-metadata.db \
+  --input data/input_files/Gap_list_all_updated.csv \
+  --delimiter ";" \
+  --log-level DEBUG

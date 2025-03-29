f6e8fef9db08df4e217079e42537226fea86a3eb
Rutger Vos <rutgeraldo@gmail.com>
2025-03-29 18:09:37 +0100

    feat: update make_bge_db.sh to fetch additional taxonomy and voucher data

--- make_bge_db.sh
+++ make_bge_db.sh
@@ -9,19 +9,23 @@ if [ ! -d "data/sqlite/ignored" ]; then
   mkdir -p data/sqlite/ignored
 fi
 
-# remove the file if it exists
+# remove the files if they exist
 rm -f data/sqlite/ignored/arise-barcode-metadata.db
+rm -f data/input_files/Gap_list_all_updated.csv
+rm -f data/input_files/all_specs_and_syn.csv
+rm -f data/input_files/taxonomy.tsv
+rm -f data/input_files/voucher.tsv
 
 # create the database
 python src/arise/barcode/metadata/util/arise_create_barcode_metadata_db.py \
   -outfile data/sqlite/ignored/arise-barcode-metadata.db
 
-# fetch the backbone data using curl from:
+# fetch the target list data using curl from:
 # https://raw.githubusercontent.com/bge-barcoding/gaplist-data/refs/heads/main/data/Gap_list_all_updated.csv
 curl -L https://raw.githubusercontent.com/bge-barcoding/gaplist-data/refs/heads/main/data/Gap_list_all_updated.csv \
   -o data/input_files/Gap_list_all_updated.csv
 
-# load the backbone data
+# load the target list data
 python src/arise/barcode/metadata/util/bge_load_targetlist.py \
   --db data/sqlite/ignored/arise-barcode-metadata.db \
   --input data/input_files/Gap_list_all_updated.csv \
@@ -32,3 +36,27 @@ python src/arise/barcode/metadata/util/bge_load_targetlist.py \
 # https://raw.githubusercontent.com/bge-barcoding/gaplist-data/refs/heads/main/data/all_specs_and_syn.csv
 curl -L https://raw.githubusercontent.com/bge-barcoding/gaplist-data/refs/heads/main/data/all_specs_and_syn.csv \
   -o data/input_files/all_specs_and_syn.csv
+
+# load the synonyms data
+python src/arise/barcode/metadata/util/bge_load_synonyms.py \
+  --db data/sqlite/ignored/arise-barcode-metadata.db \
+  --input data/input_files/all_specs_and_syn.csv \
+  --delimiter ";" \
+  --log-level DEBUG
+
+# fetch the BOLD taxonomy data (for Species, Identifier) using curl from:
+# https://raw.githubusercontent.com/bge-barcoding/gaplist-data/refs/heads/main/data/bold/taxonomy.tsv
+# Mapping:
+# `Species` -> lookup in nsr_synonym table to fetch `species_id`
+curl -L https://raw.githubusercontent.com/bge-barcoding/gaplist-data/refs/heads/main/data/bold/taxonomy.tsv \
+  -o data/input_files/taxonomy.tsv
+
+# fetch the BOLD voucher data using curl from:
+# https://raw.githubusercontent.com/bge-barcoding/gaplist-data/refs/heads/main/data/bold/voucher.tsv
+# Mapping:
+# `Sample ID` -> `sampleid`
+# `Museum ID` -> `catalognum` (if empty, use `Field ID`)
+# `Institution Storing` -> `institution_storing`
+# set locality to BGE
+curl -L https://raw.githubusercontent.com/bge-barcoding/gaplist-data/refs/heads/main/data/bold/voucher.tsv \
+  -o data/input_files/voucher.tsv
